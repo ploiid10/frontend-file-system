3cff759e99b1bd843c14f44fc24e1244c82add2c
ploiid10 <flloyd.dev@gmail.com>
2024-11-20 15:19:20 +0800

    Add changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000', // Backend URL
-  // baseURL: 'http://142.93.108.126:3000', // Backend URL
-
+  // baseURL: 'http://localhost:3000', // Backend URL
+  baseURL: 'http://142.93.108.126:3000', // Backend URL
 });
 
 // Add JWT token to headers
